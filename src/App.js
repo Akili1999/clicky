ee659449de618bc249acaec4f16451c6b6efc312
Akili1999 <sluground@gmail.com>
2019-08-13 20:23:45 -0400

    rewriting App.js

--- src/App.js
+++ src/App.js
@@ -8,59 +8,5 @@ import NavBar from "./components/NavBar";
 
 import artists from "./artists.json";
 
-class App extends Component {
-    state = {
-      artists,
-      score: 0,
-      highscore: 0
-    };
-
-    losses = () => {
-        if (this.state.score > this.state.highscore) {
-          this.setState({highscore: this.state.score}, function() {
-            console.log(this.state.highscore);
-          });
-        }
-        this.state.artists.forEach(card => {
-          card.number = 0;
-        });
-        this.setState({score: 0});
-        return true;
-      }
-
-      clickNumber = id => {
-        this.state.artists.find((j, i) => {
-          if (j.id === id) {
-            if(artists[i].number === 0){
-              artists[i].number = artists[i].number + 1;
-              this.setState({score : this.state.score + 1}, function(){
-                console.log(this.state.score);
-              });
-              this.state.artists.sort(() => Math.random() - 0.5)
-              return true; 
-            } else {
-              this.losses();
-            }
-          }
-        });
-      }
-
-      render() {
-        return (
-          <Wrapper>
-            <NavBar score={this.state.score} highscore={this.state.highscore}>Music Clicky</NavBar>
-            {this.state.artists.map(card => (
-              <Card
-                clickNumber={this.clickNumber}
-                id={card.id}
-                key={card.id}
-                image={card.image}
-              />
-            ))}
-          </Wrapper>
-        );
-      }
-    }
-    
 
 export default App;
