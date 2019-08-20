1bb460bc7b3ddcbd679a7475913e65f57feb6644
Akili1999 <sluground@gmail.com>
2019-08-20 17:47:41 -0400

    rewrite completed

--- src/App.js
+++ src/App.js
@@ -17,38 +17,33 @@ class App extends Component { //setting the state of our app
       guessed: [] // This is an array which will house all of our guessed pictures
     };
     
-    shuffle = id => { // This will shuffle both arrays so that all cards on the screen will move
-
-    if(this.state.guessed.includes(id)){
-      this.setState({ guessed: [], score: 0 });
+    handleOnClick = id => { // This is the main function for the game
+      // we are passing in the id of our cards, so that we can later control where they go
+      // Once a card is clicked, it will be stored into the array of Guesses
+      // If a user guesses a card id that exists in the Guesses array, the user loses
+    if(this.state.guessed.includes(id)){ 
+      this.Losses();
       return;
     } else {
-      this.state.guessed.push(id)
+      this.state.guessed.push(id); //If the user makes a successful guess, we push the card with the corresponding Id, into the guessed array
+      this.setState({ score: this.state.score + 1}); // And then grant a point to the user
+      for (let i = artists.length - 1; i > 0; i--) { // To shuffle the cards, we are gonna loop through the artists array
+        var shuffle = Math.floor(Math.random() * (i + 0.5)); // This varible, can be applied to our array, allowing us to take
+        // A random card, picked by math.random, and swap it with the index of artists. Thus, shuffling the array for the user to guess again
+        [artists[i], artists[shuffle]] = [artists[shuffle], artists[i]]
+      }
+
+      if(this.state.score > this.state.highscore){ //If the score that we have currently scored is higher
+        //than our previous best, then we will update the nav bar so show
+        this.setState({highscore: this.state.score})
+      }
     }
     };
 
 
-    NewHigh = () => { // If the score the user reached is higher than the current high score, then we will change
-      // The high score to the previous record
-        this.setState({highscore: this.state.score})
-    };
-
-    handleOnClick(clickedCard) {
-      const shuffledArray = this.shuffle();
-      if (!this.state.guessed.includes(clickedCard)){
-        this.state.score++;
-        shuffledArray.push(clickedCard);
-      }
-      this.state.guessed.push(clickedCard);
-
-      if (this.state.guessed.includes(clickedCard)){
-        this.state.score = 0;
-        return this.setState({
-          score: this.state.score,
-          highscore: this.state.highscore,
-          guessed: []
-        })
-      }
+    Losses = () => {
+      this.setState({ guessed: [], score: 0 }); // if the user losses, the array to store guesses will be emptied
+      // And the score will be reset
     }
 
       render() { //rendering the cards, and assigning them all of the properties we need
